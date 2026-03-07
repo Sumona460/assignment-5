dce7412861e488716eccd9d6bee80b064d6ba8b1
unknown <asumona884@gmail.com>
2026-03-07 14:38:10 +0600

    added event on signIn buttons

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const signBtn = document.getElementById('signBtn');
+
+signBtn.addEventListener('click', function (){
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+
+const defaultUser = 'admin';
+const defaultPassword = 'admin123';
+
+if(username === defaultUser && password === defaultPassword){
+    alert('Signin Successful');
+
+    window.location.href = 'dashboard.html';
+}
+else{
+    alert('Invalid Username or Password');
+}
+
+});
